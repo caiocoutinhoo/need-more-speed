4b9d2be94d358e0300720b51a3fdaa7e3bacd056
caiocoutinhoo <caiomicro15@gmail.com>
2024-05-29 13:13:32 -0300

    Backgrond + UI

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -4,7 +4,11 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
+    GamePanel gp;
     public boolean upPressed, downPressed, leftPressed, rightPressed;
+    public KeyHandler (GamePanel gp){
+        this.gp = gp;
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -12,23 +16,51 @@ public class KeyHandler implements KeyListener {
     }
     @Override // Tecla Pressionada
     public void keyPressed(KeyEvent e) {
-
         int code = e.getKeyCode();
 
-        if (code == KeyEvent.VK_W){
-            upPressed = true;
+        if(gp.gameState == gp.titleState){
+            if (code == KeyEvent.VK_W){
+                gp.ui.commandNum--;
+                if(gp.ui.commandNum < 0){
+                    gp.ui.commandNum = 1;
+                }
+            }
+            if (code == KeyEvent.VK_S){
+                gp.ui.commandNum++;
+                if(gp.ui.commandNum > 1){
+                    gp.ui.commandNum = 0;
+                }
+            }
+            if(code == KeyEvent.VK_ENTER){
+                if(gp.ui.commandNum == 0){
+                    gp.gameState = gp.playState;
+                }
+                if(gp.ui.commandNum == 1){
+                    System.exit(0);
+                }
+            }
         }
-        if (code == KeyEvent.VK_S){
+
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+            }
+        if (code == KeyEvent.VK_S) {
             downPressed = true;
         }
-        if (code == KeyEvent.VK_A){
+        if (code == KeyEvent.VK_A) {
             leftPressed = true;
         }
-        if (code == KeyEvent.VK_D){
+        if (code == KeyEvent.VK_D) {
             rightPressed = true;
+        }
+        if (code == KeyEvent.VK_P){
+            if(gp.gameState == gp.playState){
+                gp.gameState = gp.pauseState;
+            }else if(gp.gameState == gp.pauseState){
+                gp.gameState = gp.playState;
+            }
 
         }
-
     }
 
     @Override // Tecla Não-Pressionada
