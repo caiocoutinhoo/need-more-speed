6551d41e7b6325e8ee89b0f2c22073fe55590661
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2024-09-02 14:29:00 -0300

    add: contagem de tempo, de pontos e configuração de fontes

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,6 +3,8 @@ import entity.Player;
 import map.Map1;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 
 public class GamePanel extends JPanel implements Runnable {
@@ -26,28 +28,50 @@ public class GamePanel extends JPanel implements Runnable {
     public final int playState = 1;
     public final int pauseState =2;
 
+    private long totalElapsedTime = 0;  // Tempo total acumulado
+    private long pauseTime = 0;         // Tempo em que o jogo foi pausado
+    private boolean isPaused = false;   // Controle de pausa
+    private long currentTime;
+
+    private Font customFont;
+
+
     public GamePanel(){
         this.setPreferredSize( new Dimension (D_W, D_H));
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
 
+        try{    // carregando fonte personalizada
+            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("res/font/jgs_Font.ttf")).deriveFont(36f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(customFont);
+        }
+        catch (IOException | FontFormatException e){
+            e.printStackTrace();
+            customFont = new Font("Serif", Font.PLAIN, 24);
+        }
 
     }
+
+
     public void starGameThread(){
         gameThread = new Thread(this);
         gameThread.start();
         gameState = titleState;
     }
+
+
     @Override
     public void run() {
 
         this.requestFocus();
 
-        double drawnInterval = (double) 1000000000 /FPS;
+        double drawnInterval = (double) 1000000000 / FPS;
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
+
 
         while (gameThread != null){
             //FPS
@@ -66,11 +90,29 @@ public class GamePanel extends JPanel implements Runnable {
     }
     public void update(){
         if(gameState == playState){
+            if(isPaused){
+                isPaused = false; //reseta pausa
+            }
+            else{
+                // acumula o tempo do ultimo frame
+                totalElapsedTime += System.nanoTime() - currentTime;
+            }
+
+            //atualiza o currentTime para o próximo frame
+            currentTime = System.nanoTime();
 
             map1.update();
             player.update();
-        }if(gameState == pauseState){
-
+        }
+        else if(gameState == pauseState){
+            if(!isPaused){
+                pauseTime = System.nanoTime();
+                isPaused = true;
+            }
+        }
+        else if (gameState == titleState) {
+            // currentTime só é iniciado quando entrar no playState
+            currentTime = System.nanoTime();
         }
     }
     public void paintComponent(Graphics g){
@@ -79,13 +121,24 @@ public class GamePanel extends JPanel implements Runnable {
 
         if (gameState == titleState){
             ui.draw(g2);
-        }else{
+        }
+        else{
             map1.draw(g2);
             //UI
             ui.draw(g2);
+
+            g.setFont(customFont);
+
+            //conta o tempo em segundos
+            float elapsedTimeInSeconds = totalElapsedTime / 1_000_000_000.0f;
+
+            //marcação do tempo
+            g.drawString("TEMPO: " + String.format("%.2f", elapsedTimeInSeconds) + "s", 15, 42);
+
             int score = 0;
-            g.setFont(new Font("TimesRoman", Font.PLAIN, 24));
-            g.drawString("Pontos: " + score, 15, 42);
+            //marcação dos pontos
+            g.drawString("PONTOS: " + score, 15, 80);
+
         }
         g2.dispose();
     }
